54a1e7216191d7b6041257eaee64140b30a132c1
Olivia Malone <omalone@uchicago.edu>
2017-01-06 20:53:54 -0600

    made Imp public class

--- src/java/uchidb/HW0Runner.java
+++ src/java/uchidb/HW0Runner.java
@@ -8,20 +8,22 @@ import java.util.*;
  */
 
 
-public class HW0Runner extends Imp{
+public class HW0Runner {
 	// This class is a factory for a singleton containers class.
 	// https://www.tutorialspoint.com/java/java_using_singleton.htm
 
 	//TODO you likely will need to add member variable
 		//Added member variable containers
-	private static HW0Runner containers = null;
+	private static Imp containers= null;
 
-	private HW0Runner() {}
+	private HW0Runner() {
+	}
 
 	public static Containers<Integer, String> getContainers() {
 		// TODO fix this function
-
-		return containers;
+        if (containers == null)
+			containers = new Imp<Integer,String>();
+        return containers;
 	}
 
 	public static void main(String[] args){
